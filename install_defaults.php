138ed01631ea6e25ff10c843d5959e790f848f3c
Lee Garner <lee@leegarner.com>
2018-10-04 12:10:52 -0700

    Use new method for creating and syncing config items

--- install_defaults.php
+++ install_defaults.php
@@ -3,9 +3,9 @@
 *   Installation defaults for the Donation plugin.
 *
 *   @author     Lee Garner <lee@leegarner.com>
-*   @copyright  Copyright (c) 2009-2017 Lee Garner <lee@leegarner.com>
+*   @copyright  Copyright (c) 2009-2018 Lee Garner <lee@leegarner.com>
 *   @package    donation
-*   @version    0.0.1
+*   @version    0.0.3
 *   @license    http://opensource.org/licenses/gpl-2.0.php 
 *   GNU Public License v2 or later
 *   @filesource
@@ -16,52 +16,84 @@ if (!defined('GVERSION')) {
 }
 
 /**
- *  Daily Quote default settings
+ *  Default settings for the Donation plugin.
  *
  *  Initial Installation Defaults used when loading the online configuration
  *  records. These settings are only used during the initial installation
  *  and not referenced any more once the plugin is installed
- *  @global array $_DON_DEFAULT
  *
+ *  @global array $donationConfigData;
  */
-global $_DON_DEFAULT, $_CONF_DON;
-$_DON_DEFAULT = array(
-    'pp_use_donation' => 0,
+global $donationConfigData;
+$donationConfigData = array(
+    array(
+        'name' => 'sg_main',
+        'default_value' => NULL,
+        'type' => 'subgroup',
+        'subgroup' => 0,
+        'fieldset' => 0,
+        'selection_array' => NULL,
+        'sort' => 0,
+        'set' => true,
+        'group' => 'donation',
+    ),
+    array(
+        'name' => 'fs_main',
+        'default_value' => NULL,
+        'type' => 'fieldset',
+        'subgroup' => 0,
+        'fieldset' => 0,
+        'selection_array' => NULL,
+        'sort' => 0,
+        'set' => true,
+        'group' => 'donation',
+    ),
+    array(
+        'name' => 'pp_use_donation',
+        'default_value' => 0,
+        'type' => 'select',
+        'subgroup' => 0,
+        'fieldset' => 0,
+        'selection_array' => 3,
+        'sort' => 10,
+        'set' => true,
+        'group' => 'donation',
+    ),
 );
 
 
 /**
- *  Initialize Daily Quote plugin configuration
+ *  Initialize Donation plugin configuration
  *
- *  Creates the database entries for the configuation if they don't already
- *  exist. Initial values will be taken from $_CONF_DON if available (e.g. from
- *  an old config.php), uses $_DON_DEFAULT otherwise.
+ *  Creates the database entries for the configuation if they don't exist.
  *
  *  @param  integer $group_id   Group ID to use as the plugin's admin group
  *  @return boolean             true: success; false: an error occurred
  */
 function plugin_initconfig_donation($group_id = 0)
 {
-    global $_CONF, $_CONF_DON, $_DON_DEFAULT;
-
-    if (is_array($_CONF_DON) && (count($_CONF_DON) > 1)) {
-        $_DON_DEFAULT = array_merge($_DON_DEFAULT, $_CONF_DON);
-    }
+    global $donationConfigData;
 
     $c = config::get_instance();
-
-    if (!$c->group_exists($_CONF_DON['pi_name'])) {
-
-        $c->add('sg_main', NULL, 'subgroup', 0, 0, NULL, 0, true, 
-                $_CONF_DON['pi_name']);
-        $c->add('fs_paypal', NULL, 'fieldset', 0, 0, NULL, 0, true, 
-                $_CONF_DON['pi_name']);
-
-        $c->add('pp_use_donation', $_DON_DEFAULT['pp_use_donation'],
-                'select', 0, 0, 3, 10, true, $_CONF_DON['pi_name']);
+    if (!$c->group_exists('donation')) {
+        USES_lib_install();
+        foreach ($donationConfigData AS $cfgItem) {
+            _addConfigItem($cfgItem);
+        }
     }
-
     return true;
+}
+
+
+/**
+ * Sync the configuration in the DB to the above configs
+ */
+function plugin_updateconfig_donation()
+{
+    global $donationConfigData;
+
+    USES_lib_install();
+    _update_config('donation', $donationConfigData);
 }
 
 ?>
